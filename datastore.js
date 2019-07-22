f66930426a42ec4b96d8ecf6e49ca64a44647a2f
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2019-07-22 15:48:17 -0700

    adding datastore

---
+++ datastore.js
@@ -0,0 +1,106 @@
+const STORE= [
+    {
+        question: '1. Which wine usually pairs best with seafood?',
+        answers: [
+            "A. Red wine", 
+            "B. Sparkling wine",
+            "C. White wines",
+            "D. B & C"
+          ],
+        correctAnswer: "D. B & C"
+    },
+    {
+        question: "2. What temperature should champagne typically be served at (Fahrenheit)?",
+        answers: [
+            60,
+            45,
+            35,
+            65],
+        correctAnswer: 45
+    },
+    { 
+        question: "3. Where does tannin come from?",
+        answers: [
+            "A. Grape Skins",
+            "B. Wooden Barrels",
+            "C. Grape seeds & stems",
+            "D. All of the above"
+        ],
+        correctAnswer: "D. All of the above"
+    },
+    {
+        question: "4. California winemaking first became popular with which type of wine?",
+        answers: [
+            "A. Zinfindel",
+            "B. Rosé",
+            "C. Malbec",
+            "D. Pinot Grigio"
+        ],
+        correctAnswer: "A. Zinfindel"
+    },
+    {
+        question: "5. Champagne can be made in:",
+        answers: [
+            "A. Only Champagne, FR",
+            "B. Across Europe",
+            "C. Anywhere Pinot Nior vines grow",
+            "D. In France and Italy only"
+        ],
+        correctAnswer: "A. Only Champagne, FR"
+    },
+
+    { question: "6. Napa Valley is best known for the following wines:",
+    answers: [
+        "A. Chardonnay & Cabernet Sauvignon",
+        "B. Sparkling wine & Zinfindel",
+        "C. Malbec & Chardonnay",
+        "D. None, Napa is not a good climate for wine"
+    ],
+        correctAnswer: "A. Chardonnay & Cabernet Sauvignon"
+    },
+    {
+        question: "7. Noble rot is: ",
+        answers: [
+            "A. What happens when wine turns bad",
+            "B. Sometimes happens to unpicked overripe grapes",
+            "C. Can happen in the wine making process, if they don’t put enough sugar in the wine",
+            "D. Is the name of a popular California winery"
+        ],
+        correctAnswer: "B. Sometimes happens to unpicked overripe grapes"
+
+    },
+    {
+        question: "8. Chardonnay is made with ______ barrels: ",
+        answers: [
+            "A. Stainless steel",
+            "B. Plastic",
+            "C. Oak",
+            "D. Any, it depends on the wine maker’s preference "
+        ],
+        correctAnswer: "D. Any, it depends on the wine maker’s preference "
+
+    },
+    {
+        question: "9.  Unopened wine should typically be stored:",
+        answers: [
+            "A. On its side",
+            "B. Upright",
+            "C. It does not matter, as log as it is in a cool/dark place ",
+            "D. It depends on the type of wine"
+        ],
+        correctAnswer: "A. On its side"
+
+    },
+    {
+        question: "10. Sauternes is an example of: ",
+        answers: [
+            "A. Fortified wine",
+            "B. A popular California winery ",
+            "C. Sweet wine",
+            "D. Full-bodied red wine" 
+        ],
+        correctAnswer: "C. Sweet wine"
+
+    }
+
+]
