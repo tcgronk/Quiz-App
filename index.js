d65158a3cc5799fa7dd7d3500ee61b512bcdae6d
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2019-07-22 23:50:40 -0700

    udpating draft

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function generateQuestion () {
     <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
     <span>${STORE[questionNumber].answers[3]}</span>
     </label>
-    <button type="submit" class="submitButton">Submit</button>
+    <button type="submit" class="submitButton"><h3>Submit</h3></button>
     </fieldset>
     </form>
     </div>`;
@@ -98,12 +98,12 @@ function ifAnswerIsWrong () {
 
 function userAnswerFeedbackCorrect () {
   let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
-  $('.questionAnswerForm').html(`<div class="correctFeedback"><div class="icon"><i class="fas fa-wine-glass-alt"></i></div><p><b>You got it right!</b></p><button type=button class="nextButton">Next</button></div>`);
+  $('.questionAnswerForm').html(`<div class="correctFeedback"><div class="icon"><i class="fas fa-wine-glass-alt"></i></div><p><b>You got it right!</b></p><button type=button class="nextButton"><h3>Next</h3></button></div>`);
 }
 
 function userAnswerFeedbackWrong () {
   let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
-  $('.questionAnswerForm').html(`<div class="correctFeedback"><div class="icon"><i class="fas fa-wine-glass-alt"></i></div><p class="animatedShake"><b>You got it wrong</b><br>the correct answer is <span>"${correctAnswer}"</span></p><button type=button class="nextButton">Next</button></div>`);
+  $('.questionAnswerForm').html(`<div class="correctFeedback"><div class="icon"><i class="fas fa-wine-glass-alt"></i></div><p class="animatedShake"><b>You got it wrong</b><br>the correct answer is <span>${correctAnswer}</span></p><button type=button class="nextButton"><h3>Next</h3></button></div>`);
 }
 
 
@@ -114,14 +114,14 @@ function updateScore () {
 
 function renderResults () {
   if (score >= 9) {
-    $('.questionAnswerForm').html(`<div class="results correctFeedback"><i class="fas fa-award"></i> <h3>You just might be on your way to becoming a "Master Sommelier!"</h3><p>You got ${score} out of 10</p><p>You're an expert on all things wine making, storage, and pairing!</p><button class="restartButton">Restart Quiz</button></div>`);
+    $('.questionAnswerForm').html(`<div class="results correctFeedback"><i class="fas fa-award"></i> <h3>You just might be on your way to becoming a "Master Sommelier!"</h3><p>You got ${score} out of 10</p><p>You're an expert on all things wine making, storage, and pairing!</p><button class="restartButton"><h3>Restart Quiz</h3></button></div>`);
   } else if (score < 9 && score >6) {
-    $('.questionAnswerForm').html(`<div class="results correctFeedback"><i class="fas fa-glass-cheers"></i><h3>You got "Sommelier in Training!"</h3><p>You got ${score} out of 10</p><p>Keep studying your wine education to become a master somm!</p><button class="restartButton">Restart Quiz</button></div>`);
+    $('.questionAnswerForm').html(`<div class="results correctFeedback"><i class="fas fa-glass-cheers"></i><h3>You got "Sommelier in Training!"</h3><p>You got ${score} out of 10</p><p>Keep studying your wine education to become a master somm!</p><button class="restartButton"><h3>Restart Quiz</h3></button></div>`);
   } else if (score ===5 || score ===6){
-      $(".questionAnswerForm").html(`<div class="results correctFeedback"><i class="fas fa-thumbs-up"></i><h3>You got "Wine Rookie"</h3><p>You got ${score} out of 10</p><p>With more studying, you'll be on your way to mastering all things wine education!</p><button class="restartButton">Restart Quiz</button></div>`);
+      $(".questionAnswerForm").html(`<div class="results correctFeedback"><i class="fas fa-thumbs-up"></i><h3>You got "Wine Rookie"</h3><p>You got ${score} out of 10</p><p>With more studying, you'll be on your way to mastering all things wine education!</p><button class="restartButton"><h3>Restart Quiz</h3></button></div>`);
   }
     else {
-    $('.questionAnswerForm').html(`<div class="results correctFeedback"><i class="fas fa-question"></i><h3>You're a "Wine newbie!"</h3><p>You got ${score} out of 10</p><p>With more studying, you'll be on your way to mastering all things wine education!</p><button class="restartButton">Restart Quiz</button></div>`);
+    $('.questionAnswerForm').html(`<div class="results correctFeedback"><i class="fas fa-question"></i><h3>You're a "Wine newbie!"</h3><p>You got ${score} out of 10</p><p>With more studying, you'll be on your way to mastering all things wine education!</p><button class="restartButton"><h3>Restart Quiz</h3></button></div>`);
   }
 }
 
